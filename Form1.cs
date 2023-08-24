7d93bd9f0d61bf8a8d863292ee4adaeab404ecb6
masum035 <abdullahmasum6035@gmail.com>
2023-08-24 14:47:19 +0600

    audio file normalized...

--- Form1.cs
+++ Form1.cs
@@ -20,18 +20,43 @@ namespace TTSIntegration
             InitializeComponent();
         }
 
+        private static float MaxAbs(float[] data)
+        {
+            float max = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (Math.Abs(data[i]) > max)
+                {
+                    max = Math.Abs(data[i]);
+                }
+            }
+            return max;
+        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
             string inputData = "দক্ষিণ এশিয়ার প্রাচীন ও ধ্রুপদী যুগে বাংলাদেশ অঞ্চলটিতে বঙ্গ, পুণ্ড্র, গৌড়, গঙ্গাঋদ্ধি, সমতট ও হরিকেল নামক জনপদ";
             
-            float[] xyz = ONNXModelRunner.BanglaAudioGetter(inputData);
+            float[] wavData = ONNXModelRunner.BanglaAudioGetter(inputData);
             
-            var waveFormat = new WaveFormat(sampleRate:22050, channels:1);
-            
-            using (var writer = new WaveFileWriter("output.wav", waveFormat))
+            // Normalize the WAV data
+            float maxAbsValue = Math.Max(0.01f, MaxAbs(wavData));
+            short[] wavNormalized = new short[wavData.Length];
+            for (int i = 0; i < wavData.Length; i++)
             {
-                writer.WriteSamples(samples:xyz, offset:0, count:xyz.Length);
+                wavNormalized[i] = (short)(wavData[i] * (32767 / maxAbsValue));
             }
+            
+            using (var waveFileWriter = new WaveFileWriter("normalized_output.wav", new WaveFormat(22050, 16, 1)))
+            {
+                waveFileWriter.WriteSamples(wavNormalized, 0, wavNormalized.Length);
+            }
+            
+            //
+            // using (var writer = new WaveFileWriter("output.wav", waveFormat))
+            // {
+            //     writer.WriteSamples(samples:xyz, offset:0, count:xyz.Length);
+            // }
         }
     }
 }
