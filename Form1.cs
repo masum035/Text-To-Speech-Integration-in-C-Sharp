639be18aac813b82a0e05caa68f925c12d6a2ea3
masum035 <abdullahmasum6035@gmail.com>
2023-08-25 00:17:21 +0600

    text slicing added

--- Form1.cs
+++ Form1.cs
@@ -30,28 +30,58 @@ namespace TTSIntegration
                     max = Math.Abs(data[i]);
                 }
             }
+
             return max;
         }
-        
-        private void button1_Click(object sender, EventArgs e)
+
+        public static void ConcatenateAndSave(List<float[]> audioDataList, string path, int sampleRate = 22050)
         {
-            string inputData = "দক্ষিণ এশিয়ার প্রাচীন ও ধ্রুপদী যুগে বাংলাদেশ অঞ্চলটিতে বঙ্গ, পুণ্ড্র, গৌড়, গঙ্গাঋদ্ধি, সমতট ও হরিকেল নামক জনপদ";
-            
-            float[] wavData = ONNXModelRunner.BanglaAudioGetter(inputData);
-            
-            // Normalize the WAV data
-            float maxAbsValue = Math.Max(0.01f, MaxAbs(wavData));
-            short[] wavNormalized = new short[wavData.Length];
-            for (int i = 0; i < wavData.Length; i++)
+            // Concatenate the multiple arrays of audio data into a single array
+            int totalLength = 0;
+            foreach (var data in audioDataList)
             {
-                wavNormalized[i] = (short)(wavData[i] * (32767 / maxAbsValue));
+                totalLength += data.Length;
             }
             
-            using (var waveFileWriter = new WaveFileWriter("normalized_output.wav", new WaveFormat(22050, 16, 1)))
+            float[] concatenatedData = new float[totalLength];
+            int offset = 0;
+            foreach (var data in audioDataList)
+            {
+                Array.Copy(data, 0, concatenatedData, offset, data.Length);
+                offset += data.Length;
+                
+            }
+
+            // Save the concatenated audio data as a WAV file
+            // Normalize the WAV data
+            float maxAbsValue = Math.Max(0.01f, MaxAbs(concatenatedData));
+            short[] wavNormalized = new short[concatenatedData.Length];
+            for (int i = 0; i < concatenatedData.Length; i++)
+            {
+                wavNormalized[i] = (short)(concatenatedData[i] * (32767 / maxAbsValue));
+            }
+
+            using (var waveFileWriter = new WaveFileWriter(path, new WaveFormat(sampleRate, 16, 1)))
             {
                 waveFileWriter.WriteSamples(wavNormalized, 0, wavNormalized.Length);
             }
-            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string inputData = "দারিদ্রপীড়িত বাংলাদেশে অপুষ্টি একটি দুরূহ সমস্যা যা স্বাস্থ্য খাতে ব্যাপকভাবে প্রভাব বিস্তার করেছে। অপুষ্টিজনিত কারণে বাংলাদেশের ভবিষ্যৎ প্রজন্ম হিসাবে পরিচিত শিশুরা বিশ্ব ব্যাংকের জরীপে বিশ্বে শীর্ষস্থান দখল করেছে যা মোটেই কাঙ্ক্ষিত নয়। মোট জনগোষ্ঠীর ২৬% অপুষ্টিতে ভুগছে। ৪৬% শিশু মাঝারি থেকে গভীরতর পর্যায়ে ওজনজনিত সমস্যায় ভুগছে। ৫ বছর বয়সের পূর্বেই ৪৩% শিশু মারা যায়। প্রতি পাঁচ শিশুর একজন ভিটামিন এ এবং প্রতি দুইজনের একজন রক্তস্বল্পতাজনিত রোগে ভুগছে। ";
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            string inputData =
+                "প্রাকৃতিক রূপবৈচিত্র্যে ভরা আমাদের এই বাংলাদেশ। এই দেশে পরিচিত অপরিচিত অনেক পর্যটক-আকর্ষক স্থান আছে। এর মধ্যে প্রত্নতাত্ত্বিক নিদর্শন, ঐতিহাসিক মসজিদ এবং মিনার, পৃথিবীর দীর্ঘতম প্রাকৃতিক সমুদ্র সৈকত, পাহাড়, অরণ্য ইত্যাদি অন্যতম। এদেশের প্রাকৃতিক সৌন্দর্য পর্যটকদের মুগ্ধ করে। বাংলাদেশের প্রত্যেকটি এলাকা বিভিন্ন স্বতন্ত্র্র বৈশিষ্ট্যে বিশেষায়িত । বাংলাদেশ দক্ষিণ এশিয়ার উত্তর পূর্ব অংশে অবস্থিত। বাংলাদেশের উত্তর সীমানা থেকে কিছু দূরে হিমালয় পর্বতমালা এবং দক্ষিণে বঙ্গোপসাগর। পশ্চিমে ভারতের পশ্চিমবঙ্গ, পূর্বে ভারতের ত্রিপুরা, মিজোরাম রাজ্য এবং মায়ানমারের পাহাড়ী এলাকা। অসংখ্য নদ-নদী পরিবেষ্টিত বাংলাদেশ প্রধানত সমতল ভূমি। দেশের উল্লেখযোগ্য নদ-নদী হলো- পদ্মা, ব্রহ্মপুত্র, সুরমা, কুশিয়ারা, মেঘনা ও কর্ণফুলী। একেকটি অঞ্চলের প্রাকৃতিক সৌন্দর্য ও খাদ্যাভ্যাস বিভিন্ন ধরনের। বাংলাদেশ রয়েল বেঙ্গল টাইগারের দেশ যার বাস সুন্দরবনে। এছাড়াও এখানে রয়েছে লাল মাটি দিয়ে নির্মিত মন্দির। এদেশে উল্লেখযোগ্য পর্যটন এলাকার মধ্যে রয়েছে: শ্র্রীমঙ্গল, যেখানে মাইলের পর মাইল জুড়ে রয়েছে চা বাগান। প্রত্নতাত্ত্বিক নিদর্শনের স্থানগুলোর মধ্যে রয়েছে–ময়নামতি, মহাস্থানগড় এবং পাহাড়পুর। রাঙ্গামাট, কাপ্তাই এবং কক্সবাজার প্রাকৃতিক দৃশ্যের জন্য খ্যাত। সুন্দরবনে আছে বন্য প্রাণী এবং পৃথিবীখ্যাত ম্যানগ্রোভ ফরেস্ট এ বনাঞ্চলে অবস্থিত । ";
+
+            List<float[]> wavData = ONNXModelRunner.BanglaAudioGetter(inputData);
+            ConcatenateAndSave(audioDataList: wavData, path: "concatenated.wav", sampleRate: 22050);
+
+
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed Time is {0} seconds", stopwatch.ElapsedMilliseconds / 1000);
             //
             // using (var writer = new WaveFileWriter("output.wav", waveFormat))
             // {
--- ONNXModelRunner.cs
+++ ONNXModelRunner.cs
@@ -14,11 +14,10 @@ namespace BanglaTTSIntegration
     {
         public PyObject Data { get; set; }
     }
-    
+
     public class OutputData
     {
-        [ColumnName("output")] 
-        public float[][] Data { get; set; }
+        [ColumnName("output")] public float[][] Data { get; set; }
     }
 
     public class ONNXModelRunner
@@ -37,13 +36,30 @@ namespace BanglaTTSIntegration
 
                 result = pyModule.banglaTokenizer();
             }
+
             PythonEngine.Shutdown();
             return result;
         }
 
         public static float[] csArray;
-        public static float[] BanglaAudioGetter(string BanglaText)
+
+        public static List<string> SliceString(string input, int maxLength = 800)
         {
+            var slices = new List<string>();
+
+            for (int i = 0; i < input.Length; i += maxLength)
+            {
+                // The Math.Min method ensures you don't go out of bounds on the last slice
+                int length = Math.Min(maxLength, input.Length - i);
+                slices.Add(input.Substring(i, length));
+            }
+
+            return slices;
+        }
+
+        public static List<float[]> BanglaAudioGetter(string BanglaText)
+        {
+            var audioDataList = new List<float[]> { };
             Runtime.PythonDLL = @"C:\Users\Malware\AppData\Local\Programs\Python\Python311\python311.dll";
             PythonEngine.Initialize();
             using (Py.GIL())
@@ -52,49 +68,58 @@ namespace BanglaTTSIntegration
                 sys.path.append("C://Users//Malware//PycharmProjects//VitsModelTesting");
                 dynamic pyModule = Py.Import("FullModelRunner");
 
-                var banglaInput = new PyString(BanglaText);
-                dynamic result = pyModule.InvokeMethod("bangla_sentence_processing",new PyObject[] {banglaInput});
-                
-                dynamic pyList = result.tolist();
-                
-                int length = (int)pyList.Length();
-                
-                csArray = new float[length];
-                for (int i = 0; i < length; i++)
+                List<string> slices = SliceString(BanglaText);
+                foreach (string slice in slices)
                 {
-                    csArray[i] = (float) pyList[i];
+                    var banglaInput = new PyString(slice);
+                    // var banglaInput = new PyString(BanglaText);
+                    dynamic result =
+                        pyModule.InvokeMethod("bangla_sentence_processing", new PyObject[] { banglaInput });
+
+                    dynamic pyList = result.tolist();
+
+                    int length = (int)pyList.Length();
+
+                    csArray = new float[length];
+                    for (int i = 0; i < length; i++)
+                    {
+                        csArray[i] = (float)pyList[i];
+                    }
+
+                    audioDataList.Add(csArray);
                 }
             }
+
             PythonEngine.Shutdown();
-            return csArray;
+            return audioDataList;
         }
-        
+
         public void RunONNXModel()
         {
             var context = new MLContext();
-        
+
             string modelPath = "G:\\Apurba_Tech\\TTS_Model_Vits\\onnx_model\\vits_model.onnx";
             // Convert text to token IDs
             // string inputText = "দক্ষিণ এশিয়া";
             var tokenIds = GetTokenizer();
-        
+
             // Define data view
             var data = new[] { new InputData { Data = tokenIds } };
             var dataView = context.Data.LoadFromEnumerable(data);
-        
+
             // Define the pipeline
             var pipeline = context.Transforms.ApplyOnnxModel(
                 outputColumnNames: new[] { "output" },
                 inputColumnNames: new[] { "input" },
                 modelFile: modelPath);
-        
+
             // Create the transformer
             var transformer = pipeline.Fit(dataView);
             var transformedData = transformer.Transform(dataView);
-        
+
             // Get the output
             var output = context.Data.CreateEnumerable<OutputData>(transformedData, reuseRowObject: false).First();
-            
+
             Debug.WriteLine(output);
         }
     }
